9c75651b3f6272dbae009464b492b205e71c088c
Graham Ollis <plicease@cpan.org>
2018-08-22 04:02:36 -0400

    Initial commit

---
+++ lib/NewRelic/Agent/FFI.pm
@@ -0,0 +1,10 @@
+package NewRelic::Agent::FFI;
+
+use strict;
+use warnings;
+use 5.008001;
+
+# ABSTRACT: Perl Agent for NewRelic APM
+# VERSION
+
+1;
---
+++ t/newrelic_agent_ffi.t
@@ -0,0 +1,6 @@
+use Test2::V0 -no_srand => 1;
+use NewRelic::Agent::FFI;
+
+ok 1, 'todo';
+
+done_testing
