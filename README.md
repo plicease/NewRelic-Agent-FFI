056444216c81c41b46754984c2c7599c66cbef0b
Graham Ollis <plicease@cpan.org>
2018-08-23 11:17:27 -0400

    documentation

--- README.md
+++ README.md
@@ -19,18 +19,212 @@ Perl Agent for NewRelic APM
 
 # DESCRIPTION
 
-**WARNING**: This module should be considered Alpha Quality!
-
 This module provides bindings for the [NewRelic](https://docs.newrelic.com/docs/agents/agent-sdk/getting-started/new-relic-agent-sdk) Agent SDK.
 
 It is a drop in replacement for [NewRelic::Agent](https://metacpan.org/pod/NewRelic::Agent) that is implemented using [FFI::Platypus](https://metacpan.org/pod/FFI::Platypus) instead of XS and C++.
 
 Why use this module instead of the other one?
-As of this writing, you should definitely not use it in production!  See the warning above and the caveats below.
-One advantage is that this module uses powerful [Alien](https://metacpan.org/pod/Alien) technology to source the NewRelic agent libraries.  The other module
-has [a serious bug which will break when the install files are removed](https://github.com/aanari/NewRelic-Agent/issues/2).
-Another advantage to this module is that it does not require a C++ compiler, or even a C compiler for that matter.  I think
-requiring C++ is overkill for using the NewRelic SDK.
+
+- Powerful [Alien](https://metacpan.org/pod/Alien) technology
+
+    This module uses [Alien::nragent](https://metacpan.org/pod/Alien::nragent) to either download the NR agent or to use a locally installed copy.  The other module has
+    [a serious bug which will break when the install files are removed](https://github.com/aanari/NewRelic-Agent/issues/2)!  You
+    can choose the version of the NR SDK that you want to use instead of relying on the maintainer of [NewRelic::Agent](https://metacpan.org/pod/NewRelic::Agent) to do so.
+
+- Possible license issues
+
+    Related to the last point, the other module bundles the NR SDK, which may have legal risks (I am not a lawyer).  In the very least
+    I think goes against the Open Source philosophy of CPAN.
+
+- No C++ compiler required!
+
+    Since this module is built with powerful FFI and Platypus technology, you don't need to build XS bindings for it.  The
+    other module has its bindings written in C++, which is IMO unnecessary and doesn't add anything.
+
+- Tests!
+
+    The test suite for [NewRelic::Agent](https://metacpan.org/pod/NewRelic::Agent) is IMO insufficient to have confidence in it, especially if the SDK needs to be upgraded.
+    This module comes with a number of tests that will at least make sure that the calls to NewRelic will not crash your application.
+    The live test can even be configured (not on by default) to send data to NR so that you can be sure it works.
+
+- Active Development
+
+    At least as of this writing, this module is being actively developed.  The other module has a number of unanswered open issues,
+    bugs and pull requests.
+
+Why use the other module instead of this one?
+
+- This module is newer
+
+    The other module has been around for longer, and may have been used in production more.  Peoples will probably have noticed if it
+    were broken by now.
+
+# CONSTRUCTOR
+
+## new
+
+    my $agent = NewRelic::Agent::FFI->new(%options);
+
+Instantiates a new NewRelic::Agent client object.  Options include:
+
+- - `license_key`
+
+    A valid NewRelic license key for your account.
+
+    This value is also automatically sourced from the `NEWRELIC_LICENSE_KEY` environment variable.
+
+- - `app_name`
+
+    The name of your application.
+
+    This value is also automatically sourced from the `NEWRELIC_APP_NAME` environment variable.
+
+- - `app_language`
+
+    The language that your application is written in.
+
+    This value defaults to `perl`, and can also be automatically sourced from the `NEWRELIC_APP_LANGUAGE` environment variable.
+
+- - `app_language_version`
+
+    The version of the language that your application is written in.
+
+    This value defaults to your perl version, and can also be automatically sourced from the `NEWRELIC_APP_LANGUAGE_VERSION` environment variable.
+
+# METHODS
+
+Methods noted below that return `$status` return 0 for success or non-zero for failure.  See the NR SDK documentation for error codes.
+
+## embed\_collector
+
+    $agent->embed_collector;
+
+Embeds the collector agent for harvesting NewRelic data. This should be called before `init`, if the agent is being used in Embedded mode and not Daemon mode.
+
+## init
+
+    my $status = $agent->init;
+
+Initialize the connection to NewRelic.
+
+## begin\_transaction
+
+    my $tx = $agent->begin_transaction;
+
+Identifies the beginning of a transaction, which is a timed operation consisting of multiple segments. By default, transaction type is set to `WebTransaction` and transaction category is set to `Uri`.
+
+Returns the transaction's ID on success, else negative warning code or error code.
+
+## set\_transaction\_name
+
+    my $status = $agent->set_transaction_name($tx, $name);
+
+Sets the transaction name.
+
+## set\_transaction\_request\_url
+
+    my $status = $agent->set_transaction_request_url($tx, $url);
+
+Sets the transaction URL.
+
+## set\_transaction\_max\_trace\_segments
+
+    my $status = $agent->set_transaction_max_trace_segments($tx, $max);
+
+Sets the maximum trace section for the transaction.
+
+## set\_transaction\_category
+
+    my $status = $agent->set_transaction_type_web($tx);
+
+Sets the transaction type to 'web'
+
+## set\_transaction\_type\_web
+
+    my $status = $agent->set_transaction_type_other($tx);
+
+Sets the transaction type to 'other'
+
+## add\_transaction\_attribute
+
+    my $status = $agent->add_transaction_attribute($tx, $key => $value);
+
+Adds the given attribute (key/value pair) for the transaction.
+
+## notice\_transaction\_error
+
+    my $status = notice_transaction_error($tx, $exception_type, $error_message, $stack_trace, $stack_frame_delimiter);
+
+Identify an error that occurred during the transaction. The first identified
+error is sent with each transaction.
+
+## end\_transaction
+
+    my $status = $agent->end_transaction($tx);
+
+## record\_metric
+
+    my $status = $agent->record_metric($key => $value);
+
+Records the given metric (key/value pair).  The `$value` should be a floating point.
+
+## record\_cpu\_usage
+
+    my $status = $agent->record_cpu_usage($cpu_user_time_seconds, $cpu_usage_percent);
+
+Records the CPU usage. `$cpu_user_time_seconds` and `$cpu_usage_percent` are floating point values.
+
+## record\_memory\_usage
+
+    my $status = $agent->record_memory_usage($memory_megabytes);
+
+Records the memory usage. `$memory_megabytes` is a floating point value.
+
+## begin\_generic\_segment
+
+    my $seg = $agent->begin_generic_segment($tx, $parent_seg, $name);
+
+Begins a new generic segment.  `$parent_seg` is a parent segment id (`undef` no parent).  `$name` is a string.
+
+## begin\_datastore\_segment
+
+    my $seg = $agent->begin_datastore_segment($tx, $parent_seg, $table, $operation, $sql, $sql_trace_rollup_name);
+
+Begins a new datastore segment.  `$parent_seg` is a parent segment id (`undef` no parent).
+
+## begin\_external\_segment
+
+    my $seg = $agent->begin_external_segment($tx, $parent_seg, $host, $name);
+
+Begins a new external segment.  `$parent_seg` is a parent segment id (`undef` no parent).
+
+## end\_segment
+
+    my $status = $agent->end_segment($tx, $seg);
+
+End the given segment.
+
+## get\_license\_key
+
+    my $key = $agent->get_license_key;
+
+Get the license key.
+
+## get\_app\_name
+
+    my $name = $agent->get_app_name;
+
+Get the application name.
+
+## get\_app\_language
+
+    my $lang = $agent->get_app_language;
+
+Get the language name (usually `perl`).
+
+## get\_app\_language\_version
+
+Get the language version.
 
 ## CAVEATS
 
--- lib/NewRelic/Agent/FFI.pm
+++ lib/NewRelic/Agent/FFI.pm
@@ -9,6 +9,109 @@ use Alien::nragent;
 # ABSTRACT: Perl Agent for NewRelic APM
 # VERSION
 
+=head1 SYNOPSIS
+
+ use NewRelic::Agent::FFI;
+ 
+ my $agent = NewRelic:Agent::FFI->new(
+   license_key => 'abc123',
+   app_name    => 'REST API',
+ );
+ 
+ $agent->embed_collector;
+ $agent->init;
+ my $txn_id = $agent->begin_transaction;
+ ...
+ my $err_id = $agent->end_transaction($txn_id);
+
+=head1 DESCRIPTION
+
+This module provides bindings for the L<NewRelic|https://docs.newrelic.com/docs/agents/agent-sdk/getting-started/new-relic-agent-sdk> Agent SDK.
+
+It is a drop in replacement for L<NewRelic::Agent> that is implemented using L<FFI::Platypus> instead of XS and C++.
+
+Why use this module instead of the other one?
+
+=over 4
+
+=item Powerful L<Alien> technology
+
+This module uses L<Alien::nragent> to either download the NR agent or to use a locally installed copy.  The other module has
+L<a serious bug which will break when the install files are removed|https://github.com/aanari/NewRelic-Agent/issues/2>!  You
+can choose the version of the NR SDK that you want to use instead of relying on the maintainer of L<NewRelic::Agent> to do so.
+
+=item Possible license issues
+
+Related to the last point, the other module bundles the NR SDK, which may have legal risks (I am not a lawyer).  In the very least
+I think goes against the Open Source philosophy of CPAN.
+
+=item No C++ compiler required!
+
+Since this module is built with powerful FFI and Platypus technology, you don't need to build XS bindings for it.  The
+other module has its bindings written in C++, which is IMO unnecessary and doesn't add anything.
+
+=item Tests!
+
+The test suite for L<NewRelic::Agent> is IMO insufficient to have confidence in it, especially if the SDK needs to be upgraded.
+This module comes with a number of tests that will at least make sure that the calls to NewRelic will not crash your application.
+The live test can even be configured (not on by default) to send data to NR so that you can be sure it works.
+
+=item Active Development
+
+At least as of this writing, this module is being actively developed.  The other module has a number of unanswered open issues,
+bugs and pull requests.
+
+=back
+
+Why use the other module instead of this one?
+
+=over 4
+
+=item This module is newer
+
+The other module has been around for longer, and may have been used in production more.  Peoples will probably have noticed if it
+were broken by now.
+
+=back
+
+=head1 CONSTRUCTOR
+
+=head2 new
+
+ my $agent = NewRelic::Agent::FFI->new(%options);
+
+Instantiates a new NewRelic::Agent client object.  Options include:
+
+=over 4
+
+=item - C<license_key>
+
+A valid NewRelic license key for your account.
+
+This value is also automatically sourced from the C<NEWRELIC_LICENSE_KEY> environment variable.
+
+=item - C<app_name>
+
+The name of your application.
+
+This value is also automatically sourced from the C<NEWRELIC_APP_NAME> environment variable.
+
+=item - C<app_language>
+
+The language that your application is written in.
+
+This value defaults to C<perl>, and can also be automatically sourced from the C<NEWRELIC_APP_LANGUAGE> environment variable.
+
+=item - C<app_language_version>
+
+The version of the language that your application is written in.
+
+This value defaults to your perl version, and can also be automatically sourced from the C<NEWRELIC_APP_LANGUAGE_VERSION> environment variable.
+
+=back
+
+=cut
+
 sub new
 {
   my($class, %args) = @_;
@@ -43,6 +146,18 @@ my $ffi = FFI::Platypus->new;
 $ffi->lib(Alien::nragent->dynamic_libs);
 my $newrelic_basic_literal_replacement_obfuscator = $ffi->find_symbol('newrelic_basic_literal_replacement_obfuscator');
 
+=head1 METHODS
+
+Methods noted below that return C<$status> return 0 for success or non-zero for failure.  See the NR SDK documentation for error codes.
+
+=head2 embed_collector
+
+ $agent->embed_collector;
+
+Embeds the collector agent for harvesting NewRelic data. This should be called before C<init>, if the agent is being used in Embedded mode and not Daemon mode.
+
+=cut
+
 sub embed_collector
 {
   my($self) = @_;
@@ -57,6 +172,14 @@ sub embed_collector
   }
 }
 
+=head2 init
+
+ my $status = $agent->init;
+
+Initialize the connection to NewRelic.
+
+=cut
+
 $ffi->attach( [ newrelic_init => 'init' ] => [ 'string', 'string', 'string', 'string' ] => 'int' => sub {
   my($xsub, $self) = @_;
   $xsub->(
@@ -66,6 +189,16 @@ $ffi->attach( [ newrelic_init => 'init' ] => [ 'string', 'string', 'string', 'st
     $self->get_app_language_version,
   );
 });
+
+=head2 begin_transaction
+
+ my $tx = $agent->begin_transaction;
+
+Identifies the beginning of a transaction, which is a timed operation consisting of multiple segments. By default, transaction type is set to C<WebTransaction> and transaction category is set to C<Uri>.
+
+Returns the transaction's ID on success, else negative warning code or error code.
+
+=cut
 
 $ffi->attach( [ newrelic_transaction_begin => 'begin_transaction' ] => [] => 'long' => sub {
   shift->();
@@ -86,16 +219,60 @@ sub _set3
   $_[0]->(@_[2,3,4]);
 }
 
+=head2 set_transaction_name
+
+ my $status = $agent->set_transaction_name($tx, $name);
+
+Sets the transaction name.
+
+=head2 set_transaction_request_url
+
+ my $status = $agent->set_transaction_request_url($tx, $url);
+
+Sets the transaction URL.
+
+=head2 set_transaction_max_trace_segments
+
+ my $status = $agent->set_transaction_max_trace_segments($tx, $max);
+
+Sets the maximum trace section for the transaction.
+
+=head2 set_transaction_category
+
+ my $status = $agent->set_transaction_type_web($tx);
+
+Sets the transaction type to 'web'
+
+=head2 set_transaction_type_web
+
+ my $status = $agent->set_transaction_type_other($tx);
+
+Sets the transaction type to 'other'
+
+=head2 add_transaction_attribute
+
+ my $status = $agent->add_transaction_attribute($tx, $key => $value);
+
+Adds the given attribute (key/value pair) for the transaction.
+
+=cut
+
 $ffi->attach( [ newrelic_transaction_set_name               => 'set_transaction_name'               ] => [ 'long', 'string' ] => 'int' => \&_set2 );
 $ffi->attach( [ newrelic_transaction_set_request_url        => 'set_transaction_request_url'        ] => [ 'long', 'string' ] => 'int' => \&_set2 );
 $ffi->attach( [ newrelic_transaction_set_max_trace_segments => 'set_transaction_max_trace_segments' ] => [ 'long', 'int'    ] => 'int' => \&_set2 );
 $ffi->attach( [ newrelic_transaction_set_category           => 'set_transaction_category'           ] => [ 'long', 'string' ] => 'int' => \&_set2 );
-
-
 $ffi->attach( [ newrelic_transaction_set_type_web   => 'set_transaction_type_web'   ] => [ 'long' ] => 'int' => \&_set1 );
 $ffi->attach( [ newrelic_transaction_set_type_other => 'set_transaction_type_other' ] => [ 'long' ] => 'int' => \&_set1 );
-
 $ffi->attach( [ newrelic_transaction_add_attribute => 'add_transaction_attribute' ] => [ 'long', 'string', 'string' ] => 'int' => \&_set3);
+
+=head2 notice_transaction_error
+
+ my $status = notice_transaction_error($tx, $exception_type, $error_message, $stack_trace, $stack_frame_delimiter);
+
+Identify an error that occurred during the transaction. The first identified
+error is sent with each transaction.
+
+=cut
 
 $ffi->attach( [ newrelic_transaction_notice_error => 'notice_transaction_error' ] => [ 'long', 'string', 'string', 'string', 'string' ] => 'int' => sub {
   my $xsub = shift;
@@ -103,10 +280,42 @@ $ffi->attach( [ newrelic_transaction_notice_error => 'notice_transaction_error' 
   $xsub->(@_);
 });
 
+=head2 end_transaction
+
+ my $status = $agent->end_transaction($tx);
+
+=head2 record_metric
+
+ my $status = $agent->record_metric($key => $value);
+
+Records the given metric (key/value pair).  The C<$value> should be a floating point.
+
+=head2 record_cpu_usage
+
+ my $status = $agent->record_cpu_usage($cpu_user_time_seconds, $cpu_usage_percent);
+
+Records the CPU usage. C<$cpu_user_time_seconds> and C<$cpu_usage_percent> are floating point values.
+
+=head2 record_memory_usage
+
+ my $status = $agent->record_memory_usage($memory_megabytes);
+
+Records the memory usage. C<$memory_megabytes> is a floating point value.
+
+=cut
+
 $ffi->attach( [ newrelic_transaction_end => 'end_transaction' ] => [ 'long' ] => 'int' => \&_set1 );
 $ffi->attach( [ newrelic_record_metric => 'record_metric' ] => [ 'string', 'double'] => 'int' => \&_set2 );
 $ffi->attach( [ newrelic_record_cpu_usage => 'record_cpu_usage' ] => [ 'double', 'double' ] => 'int' => \&_set2);
 $ffi->attach( [ newrelic_record_memory_usage => 'record_memory_usage' ] => [ 'double' ] => 'int' => \&_set1);
+
+=head2 begin_generic_segment
+
+ my $seg = $agent->begin_generic_segment($tx, $parent_seg, $name);
+
+Begins a new generic segment.  C<$parent_seg> is a parent segment id (C<undef> no parent).  C<$name> is a string.
+
+=cut
 
 $ffi->attach( [ newrelic_segment_generic_begin => 'begin_generic_segment' ] => [ 'long', 'long', 'string' ] => 'long' => sub {
   my $xsub = shift;
@@ -114,9 +323,25 @@ $ffi->attach( [ newrelic_segment_generic_begin => 'begin_generic_segment' ] => [
   $xsub->(@_);
 });
 
+=head2 begin_datastore_segment
+
+ my $seg = $agent->begin_datastore_segment($tx, $parent_seg, $table, $operation, $sql, $sql_trace_rollup_name);
+
+Begins a new datastore segment.  C<$parent_seg> is a parent segment id (C<undef> no parent).
+
+=cut
+
 $ffi->attach( [ newrelic_segment_datastore_begin => 'begin_datastore_segment' ] => [ 'long', 'long', 'string', 'string', 'string', 'string', 'opaque' ] => 'long' => sub {
   $_[0]->(@_[2,3,4,5,6,7], $newrelic_basic_literal_replacement_obfuscator);
 });
+
+=head2 begin_external_segment
+
+ my $seg = $agent->begin_external_segment($tx, $parent_seg, $host, $name);
+
+Begins a new external segment.  C<$parent_seg> is a parent segment id (C<undef> no parent).
+
+=cut
 
 $ffi->attach( [ newrelic_segment_external_begin => 'begin_external_segment' ] => [ 'long', 'long', 'string', 'string' ] => 'long' => sub {
   my $xsub = shift;
@@ -124,7 +349,39 @@ $ffi->attach( [ newrelic_segment_external_begin => 'begin_external_segment' ] =>
   $xsub->(@_);
 });
 
+=head2 end_segment
+
+ my $status = $agent->end_segment($tx, $seg);
+
+End the given segment.
+
+=cut
+
 $ffi->attach( [ newrelic_segment_end => 'end_segment' ] => [ 'long', 'long' ] => 'int' => \&_set2);
+
+=head2 get_license_key
+
+ my $key = $agent->get_license_key;
+
+Get the license key.
+
+=head2 get_app_name
+
+ my $name = $agent->get_app_name;
+
+Get the application name.
+
+=head2 get_app_language
+
+ my $lang = $agent->get_app_language;
+
+Get the language name (usually C<perl>).
+
+=head2 get_app_language_version
+
+Get the language version.
+
+=cut
 
 sub get_license_key { shift->{license_key} }
 sub get_app_name { shift->{app_name} }
@@ -132,36 +389,6 @@ sub get_app_language { shift->{app_language} }
 sub get_app_language_version { shift->{app_language_version} }
 
 1;
-
-=head1 SYNOPSIS
-
- use NewRelic::Agent::FFI;
- 
- my $agent = NewRelic:Agent::FFI->new(
-   license_key => 'abc123',
-   app_name    => 'REST API',
- );
- 
- $agent->embed_collector;
- $agent->init;
- my $txn_id = $agent->begin_transaction;
- ...
- my $err_id = $agent->end_transaction($txn_id);
-
-=head1 DESCRIPTION
-
-B<WARNING>: This module should be considered Alpha Quality!
-
-This module provides bindings for the L<NewRelic|https://docs.newrelic.com/docs/agents/agent-sdk/getting-started/new-relic-agent-sdk> Agent SDK.
-
-It is a drop in replacement for L<NewRelic::Agent> that is implemented using L<FFI::Platypus> instead of XS and C++.
-
-Why use this module instead of the other one?
-As of this writing, you should definitely not use it in production!  See the warning above and the caveats below.
-One advantage is that this module uses powerful L<Alien> technology to source the NewRelic agent libraries.  The other module
-has L<a serious bug which will break when the install files are removed|https://github.com/aanari/NewRelic-Agent/issues/2>.
-Another advantage to this module is that it does not require a C++ compiler, or even a C compiler for that matter.  I think
-requiring C++ is overkill for using the NewRelic SDK.
 
 =head2 CAVEATS
 
